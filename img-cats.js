fb5d8f48b58454abd456277b2ff1495c85152fe1
Mervin Hernandez <me@mervinhernandez.com>
2019-06-27 12:17:27 -0400

    Updated description box styling
    added a different styling for when it's collapsed

--- img-cats.js
+++ img-cats.js
@@ -1,14 +1,23 @@
+let css = document.createElement('style');
+css.setAttribute('type', 'text/css');
+
+let rules = `
 /*
  * CSS Style Overwrites for:
  * intermediagroup.catsone.com
- * Version 2019.06.21.1300
+ * Version 2019.06.27.1215
  */
 
 /* Panel - Job Description */
 .form-columns > div > div:nth-child(6) {
     width: 1095px;
     z-index: 900;
-    box-shadow: 0;
+	box-shadow: 0px 0px 8px 0px black;
+}
+/* -- shrink back down when collapsed */
+.form-columns > div > div:nth-child(6).card-collapsed {
+	width: unset !important;
+	box-shadow: unset;
 }
 
 /* = = Global Properties = = */
@@ -45,6 +54,7 @@ li[data-field-id="companyJobID"] > div > div.field-text
 ,li[data-field-id="extraField185084"] > div > div.field-text
  {
   font-size: 1.765em;
+  line-height: 1.2em;
   font-weight: 800;
   padding: 4px !important;
   background: #f2f2f2;
@@ -118,3 +128,21 @@ div.feed-item-timestamp > span {
  {
   display:none;
 }
+`;
+
+css.appendChild(document.createTextNode(rules));
+
+document.querySelector('body').appendChild(css);
+
+window.addEventListener('load', () => {
+  let script = document.createElement('script');
+
+  script.appendChild(
+    document.createTextNode(`
+let cards = Array.from(document.querySelectorAll('.card'));
+
+cards.forEach(card => card.setAttribute('id', card.childNodes[2].innerText));
+`)
+  );
+  document.querySelector('body').appendChild(script);
+});
